dd0f911d92b15abb0aef8679fb16ef0cef4ab6a0
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-01-29 11:07:54 +0100

    removed pdist, should be ok for parfor

--- fromRGBtoValue.m
+++ fromRGBtoValue.m
@@ -1,24 +1,19 @@
-piedergb = imread('000.bmp');
-lab_image = rgb2lab(piedergb);
-lab_colors = rgb2lab(myMatrixColor(:,1:3));
-[x,y,z] = size(lab_image);
-[x1,y1] = size(lab_colors);
+piedergb = im2double(imread('000.bmp'));
+[x,y,z] = size(piedergb);
+[x1,y1] = size(myMatrixColor);
 piedeValue=zeros(x,y,z);
 
-parfor i=1:x
+for i=1:x
     for j=1:y
         min = realmax;
         minIndex = 0;
         for k=1:x1
-          deltaE = sqrt((lab_image(i,j,1)-lab_colors(k,1))^2 + (lab_image(i,j,2)-lab_colors(k,2))^2 + (lab_image(i,j,3)-lab_colors(k,3))^2);
-          if(deltaE < min)
+          diff = sqrt(sum((squeeze(piedergb(i,j,:))-myMatrixColor(k, 1:3)).^2));
+          if(diff < min)
             minIndex = k;
             min = deltaE;
           end
         end
-        piedeValue(i,j,:) = lab_colors(minIndex,:);
+        piedeValue(i,j,:) = myMatrixColor(minIndex,1:3);
     end
 end
-
-final_image = lab2rgb(piedeValue);
-
