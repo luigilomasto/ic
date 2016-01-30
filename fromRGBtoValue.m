90d24b67fbb4114b1cbde7d157ddd759efbc0556
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-01-30 08:31:55 +0100

    Added octave compatibility check

--- fromRGBtoValue.m
+++ fromRGBtoValue.m
@@ -1,24 +1,46 @@
+isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
+if isOctave
+	pkg load image
+	pkg load parallel
+end
 piedergb = im2double(imread('000.bmp'));
-%myMatrixColorDouble = im2double(myMatrixColor);
 [x,y,z] = size(piedergb);
-[x1,y1] = size(myMatrixColorDouble2);
+[x1,y1] = size(myMatrixColor);
 piedeImg=zeros(x,y,z);
 piedeValue=zeros(x,y);
 tic
-parfor i=1:x
-    for j=1:y
-        min = realmax;
-        minIndex = 0;
-        for k=1:x1
-          diff = sqrt(sum((squeeze(piedergb(i,j,:))'-myMatrixColorDouble2(k, 1:3)).^2));
-          if(diff < min)
-            minIndex = k;
-            min = diff;
-          end
-        end
-        piedeImg(i,j,:) = myMatrixColorDouble2(minIndex, 1:3);
-        piedeValue(i,j) = myMatrixColorDouble2(minIndex,4);
-    end
+if ~isOctave
+	parfor i=1:x
+	    for j=1:y
+		min = realmax;
+		minIndex = 0;
+		for k=1:x1
+		  diff = sqrt(sum((squeeze(piedergb(i,j,:))'-myMatrixColor(k, 1:3)).^2));
+		  if(diff < min)
+		    minIndex = k;
+		    min = diff;
+		  end
+		end
+		piedeImg(i,j,:) = myMatrixColor(minIndex, 1:3);
+		piedeValue(i,j) = myMatrixColor(minIndex,4);
+	    end
+	end
+else
+	for i=1:x
+	    for j=1:y
+		min = realmax;
+		minIndex = 0;
+		for k=1:x1
+		  diff = sqrt(sum((squeeze(piedergb(i,j,:))'-myMatrixColor(k, 1:3)).^2));
+		  if(diff < min)
+		    minIndex = k;
+		    min = diff;
+		  end
+		end
+		piedeImg(i,j,:) = myMatrixColor(minIndex, 1:3);
+		piedeValue(i,j) = myMatrixColor(minIndex,4);
+	    end
+	end
 end
 clear x; clear y; clear z; clear minIndex; clear min; clear diff; clear i;
 clear j; clear k; clear x1; clear y1;
