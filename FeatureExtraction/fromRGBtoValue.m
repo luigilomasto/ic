06b0e30ddee6ea3f5f73dbe5b1cdd53d264fd232
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-02-02 19:14:08 +0100

    Changed some variables names and relative paths

--- FeatureExtraction/fromRGBtoValue.m
+++ FeatureExtraction/fromRGBtoValue.m
@@ -1,4 +1,4 @@
-function fromRGBtoValue(imagePath)
+function piedeValue = fromRGBtoValue(imagePath)
 isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
 if isOctave
 	pkg load image;
@@ -6,24 +6,22 @@ if isOctave
 end
 piedergb = im2double(imread(imagePath));
 [x,y,z] = size(piedergb);
-myMatrixColor = extractColor('color.bmp');
-[x1,y1] = size(myMatrixColor);
-%piedeImg=zeros(x,y,z, 'double');
-piedeValue=zeros(x,y, 'double');
+myMatrixColor = extractColor('../color.bmp');
+[num_colors,rgb] = size(myMatrixColor);
+piedeValue=zeros(x,y,'double');
 tic
 if ~isOctave
 	parfor i=1:x
 	    for j=1:y
 		min = realmax;
 		minIndex = 0;
-		for k=1:x1
+		for k=1:num_colors
 		  diff = sqrt(sum((squeeze(piedergb(i,j,:))'-myMatrixColor(k, 1:3)).^2));
 		  if(diff < min)
 		    minIndex = k;
 		    min = diff;
 		  end
 		end
-		%piedeImg(i,j,:) = myMatrixColor(minIndex, 1:3);
 		piedeValue(i,j) = myMatrixColor(minIndex,4);
 	    end
 	end
@@ -32,24 +30,20 @@ else
 	    for j=1:y
 		min = realmax;
 		minIndex = 0;
-		for k=1:x1
+		for k=1:num_colors
 		  diff = sqrt(sum((squeeze(piedergb(i,j,:))'-myMatrixColor(k, 1:3)).^2));
 		  if(diff < min)
 		    minIndex = k;
 		    min = diff;
 		  end
 		end
-		%piedeImg(i,j,:) = myMatrixColor(minIndex, 1:3);
 		piedeValue(i,j) = myMatrixColor(minIndex,4);
 	    end
 	end
 end
 
-%imwrite(piedeImg, 'piedeClustered.bmp');
-
-%save(strcat(imagePath, '_binary'), piedeValue);
 imwrite(piedeValue, 'piedeBN.bmp')
 clear x; clear y; clear z; clear minIndex; clear min; clear diff; clear i;
-clear j; clear k; clear x1; clear y1; clear piedeValue;
+clear j; clear k; clear num_colors; clear rgb; 
 toc
 end
