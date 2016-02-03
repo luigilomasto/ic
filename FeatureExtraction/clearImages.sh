e010866ca59629f232ab94d66c167cba1914993c
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-02-03 11:02:33 +0100

    Modified script according to new file organization

--- FeatureExtraction/clearImages.sh
+++ FeatureExtraction/clearImages.sh
@@ -1,12 +1,7 @@
 #!/bin/zsh
 
-cd ~/Dati
-for dir in $(ls -d */);do
-	cd $dir
-	if [ -d 'rec000000' ]; then
-		cd rec000000
-		octave --silent --eval 'clearImage("000.bmp")'
-		cd ..
-	fi
-	cd ..
+cd ~/DatiNew
+for file in $(ls *.bmp);do
+	octave --silent --eval "clearImage(\"$file\")"
 done 
+cd -
