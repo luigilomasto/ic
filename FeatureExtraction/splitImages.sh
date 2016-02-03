72931d4fc30964233aeb610179614867357893f5
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-02-03 22:36:58 +0100

    Added script to split all bn images

---
+++ FeatureExtraction/splitImages.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+if [ -z "$1" ]; then
+	startIndex=0
+else
+	startIndex=$1
+fi
+
+if [ -z "$2" ]; then
+	endIndex=322
+else
+	endIndex=$2
+fi
+
+cd ~/Dati
+for (( i = $startIndex; i < $endIndex; i++ )); do
+	image=$i"_cleared_bn.bmp"
+	if [ -f $image ]; then
+		string="splitImages(\"$image\")"
+		octave --silent --eval "$string"
+	fi
+done 
+cd -
