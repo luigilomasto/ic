15e2ecdf173a409fa416f8548f53aa24bab92665
Luigi <luilom>
2016-02-10 15:38:34 +0100

    risolti alcuni bug

--- FeatureExtraction/FeaturesFirstClassifier.m
+++ FeatureExtraction/FeaturesFirstClassifier.m
@@ -2,7 +2,7 @@
 
 function matrix = FeaturesFirstClassifier (numImage)
 
-matrix=zeros(numImage*2,6,'double');
+matrix=zeros(numImage*2,7,'double');
 path='../DatiPreprocessed/';
 
 for i=1:numImage
@@ -15,22 +15,27 @@ try
    matrix(j,3)=lengthMediaIstmo;
    matrix(j,4)=indicePatologia;
    matrix(j,5)=mediumPressure;
+   
 catch exception
-   matrix(i,:)=0;
+   matrix(j,:)=0;
 end
 path='../DatiPreprocessed/';
 path=strcat(path,num2str(i-1),'_cleared_bn_right_rotated.png');
 try
-   [lengthMinIstmo,lengthMaxAvampiede,lengthMediaIstmo,mediumPressure] = RegionOfInterest(path);
+   [lengthMinIstmo,lengthMaxAvampiede,lengthMediaIstmo,indicePatologia,mediumPressure] = RegionOfInterest(path);
    matrix(j+1,1)=lengthMinIstmo;
    matrix(j+1,2)=lengthMaxAvampiede;
    matrix(j+1,3)=lengthMediaIstmo;
-   matrix(j+1,4)=mediumPressure;
+   matrix(j+1,4)=indicePatologia;
+   matrix(j+1,5)=mediumPressure;
+
 catch exception
-   matrix(j,:)=0;
    matrix(j+1,:)=0;
 end
 
+
+
 path='../DatiPreprocessed/';
 end
+
 end
