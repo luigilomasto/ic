235b63bc29d2aabaaeaca7984443f2980345b133
Luigi <Luigi>
2016-02-11 11:11:21 +0100

    aggiunta metodo svmMultiple

--- FeatureExtraction/prova.m
+++ FeatureExtraction/prova.m
@@ -1,6 +1,7 @@
-training=FeaturesFirstClassifier (10);
+training=FeaturesFirstClassifier (5);
 label=zeros(10);
 label=[1 1 2 2 3 3 1 1 1 1];
-SVMStruct = svmtrain(training,label);
+%SVMStruct = svmtrain(training,label');
 test=FeaturesFirstClassifier (30);
-Group = svmclassify(SVMStruct,test)
+%Group = svmclassify(SVMStruct,test);
+results = multisvm(training, label, test); 
