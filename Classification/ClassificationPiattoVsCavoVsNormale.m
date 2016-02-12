cd21617cfaeb85983ca30d8ccbabc4d459596b20
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-02-12 16:34:52 +0100

    Removed some hardcoded checks and changed some variables names

--- Classification/ClassificationPiattoVsCavoVsNormale.m
+++ Classification/ClassificationPiattoVsCavoVsNormale.m
@@ -11,38 +11,34 @@ addpath(genpath('..'));
 numRip=10;
 numFold=2;
 fullMatrix = FeaturesFirstClassifier(labelsPath, dataPath);
-featuresRange=3:7;
-perc_totale_good=0;
-accuratezza_1 = 0;
-accuratezza_2 = 0;
-accuratezza_3 = 0;
+%FeaturesRange can be changed with RFE
+featuresRange = 3:7;
+label_column = 8;
+total_accuracy = 0;
+num_classes = unique(fullMatrix(:,label_column));
+class_accuracy = zeros(length(num_classes));
 
 for i=1:numRip
-    c = cvpartition(fullMatrix(:,8),'KFold',2);
+    c = cvpartition(fullMatrix(:,label_column),'KFold',2);
     train=training(c,1);
     test=test(c,1);
     
     trainingSetRange = find(train)';
     testSetRange=find(test)';
 
-    trainingPatients = fullMatrix(trainingSetRange, 1);
     trainingSet = fullMatrix(trainingSetRange, featuresRange);
-    testPatients = fullMatrix(testSetRange, 1);
     testSet=fullMatrix(testSetRange, featuresRange);
-    label = fullMatrix(trainingSetRange, 8);
+    label = fullMatrix(trainingSetRange, label_column);
     results = multisvm(trainingSet, label', testSet);
-    real_results = fullMatrix(testSetRange, 8);
-    good_classified = (results == real_results);
-    perc_good_classified = sum(good_classified)/length(good_classified);
-    
-    perc_totale_good = perc_totale_good+perc_good_classified;
+    real_results = fullMatrix(testSetRange, label_column);
+    total_accuracy = total_accuracy + sum(results == real_results)/length(results);
     tab = crosstab(results, real_results);
-    accuratezza_1 = accuratezza_1 + tab(1,1)/sum(tab(:,1));
-    accuratezza_2 = accuratezza_2 + tab(2,2)/sum(tab(:,2));
-    accuratezza_3 = accuratezza_3 + tab(3,3)/sum(tab(:,3));
+    for j=1:num_classes
+        class_accuracy(j) = class_accuracy(j) + tab(j,j)/sum(tab(:,j));
+    end
     clear test train c;
 end
 perc_totale_good = perc_totale_good/numRip;
-accuratezza_1 = accuratezza_1/numRip;
-accuratezza_2 = accuratezza_2/numRip;
-accuratezza_3 = accuratezza_3/numRip;
+for j=1:num_classes
+    class_accuracy(j) = class_accuracy(j)/numRip;
+end
