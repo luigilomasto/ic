67faea2b18932aac04dcba00594b9e5ca603645a
Luigi <Luigi>
2016-02-12 15:44:54 +0100

    Added partial accuracy calcolation!

--- Classification/ClassificationPiattoVsCavoVsNormale.m
+++ Classification/ClassificationPiattoVsCavoVsNormale.m
@@ -8,17 +8,19 @@ end
 
 addpath(genpath('..'));
 
-numRip=5;
+numRip=10;
 numFold=2;
 fullMatrix = FeaturesFirstClassifier(labelsPath, dataPath);
 featuresRange=3:7;
-percentuale=0;
-
+perc_totale_good=0;
+accuratezza_1 = 0;
+accuratezza_2 = 0;
+accuratezza_3 = 0;
 
 for i=1:numRip
     c = cvpartition(fullMatrix(:,8),'KFold',2);
     train=training(c,1);
-    test=test(c,2);
+    test=test(c,1);
     
     trainingSetRange = find(train)';
     testSetRange=find(test)';
@@ -33,7 +35,14 @@ for i=1:numRip
     good_classified = (results == real_results);
     perc_good_classified = sum(good_classified)/length(good_classified);
     
-    percentuale = percentuale+perc_good_classified;
+    perc_totale_good = perc_totale_good+perc_good_classified;
+    tab = crosstab(results, real_results);
+    accuratezza_1 = accuratezza_1 + tab(1,1)/sum(tab(:,1));
+    accuratezza_2 = accuratezza_2 + tab(2,2)/sum(tab(:,2));
+    accuratezza_3 = accuratezza_3 + tab(3,3)/sum(tab(:,3));
     clear test train c;
 end
-percentuale = percentuale/numRip;
+perc_totale_good = perc_totale_good/numRip;
+accuratezza_1 = accuratezza_1/numRip;
+accuratezza_2 = accuratezza_2/numRip;
+accuratezza_3 = accuratezza_3/numRip;
