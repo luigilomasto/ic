aaa76b655346f404d90ce6129e525232128509c4
Luigi <Luigi>
2016-02-12 14:44:39 +0100

    Added k folding to classification

--- Classification/ClassificationPiattoVsCavoVsNormale.m
+++ Classification/ClassificationPiattoVsCavoVsNormale.m
@@ -8,17 +8,32 @@ end
 
 addpath(genpath('..'));
 
-featuresRange=3:7;
-trainingSetRange = 1:40;
-testSetRange=41:100;
-
+numRip=5;
+numFold=2;
 fullMatrix = FeaturesFirstClassifier(labelsPath, dataPath);
-trainingPatients = fullMatrix(trainingSetRange, 1);
-trainingSet = fullMatrix(trainingSetRange, featuresRange);
-testPatients = fullMatrix(testSetRange, 1);
-testSet=fullMatrix(testSetRange, featuresRange);
-label = fullMatrix(trainingSetRange, 8);
-results = multisvm(trainingSet, label', testSet);
-real_results = fullMatrix(testSetRange, 8);
-good_classified = (results == real_results);
-perc_good_classified = sum(good_classified)/length(good_classified);
+featuresRange=3:7;
+percentuale=0;
+
+
+for i=1:numRip
+    c = cvpartition(fullMatrix(:,8),'KFold',2);
+    train=training(c,1);
+    test=test(c,2);
+    
+    trainingSetRange = find(train)';
+    testSetRange=find(test)';
+
+    trainingPatients = fullMatrix(trainingSetRange, 1);
+    trainingSet = fullMatrix(trainingSetRange, featuresRange);
+    testPatients = fullMatrix(testSetRange, 1);
+    testSet=fullMatrix(testSetRange, featuresRange);
+    label = fullMatrix(trainingSetRange, 8);
+    results = multisvm(trainingSet, label', testSet);
+    real_results = fullMatrix(testSetRange, 8);
+    good_classified = (results == real_results);
+    perc_good_classified = sum(good_classified)/length(good_classified);
+    
+    percentuale = percentuale+perc_good_classified;
+    clear test train c;
+end
+percentuale = percentuale/numRip;
